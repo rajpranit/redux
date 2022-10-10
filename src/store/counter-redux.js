ba16b3b9192ac6f3aa00f51b2eb9e2c0f13e827d
rajpranit <rajpranit1207@gmail.com>
2022-10-10 09:37:47 +0530

    create slice

--- src/store/counter-redux.js
+++ src/store/counter-redux.js
@@ -1,6 +1,26 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 1, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.value;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = { counter: 1 }, action) => {
   if (action.type === "INCREMENT") {
